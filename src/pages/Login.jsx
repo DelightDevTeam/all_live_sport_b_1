d8fc092f030d0ca5fce9aac53217e74437804c94
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-04-08 10:19:12 +0630

    login integrate

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import '../assets/css/login.css'
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import BASE_URL from "../hooks/baseURL";
 
 import logo from '../assets/img/logo.png';
 // import flag from '../assets/img/flag.png';
@@ -10,6 +11,82 @@ import logo from '../assets/img/logo.png';
 // import { FiKey } from "react-icons/fi";
 
 const Login = () => {
+    const [playerId, setPlayerId] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState('');
+
+    const auth = localStorage.getItem("token");
+    const navigate = useNavigate();
+
+    if(auth){
+        useEffect(() => {
+          navigate("/"); // Navigate to the home route
+        }, [navigate]);
+    }
+
+    const login = (e) =>{
+        e.preventDefault();
+        setLoading(true);
+        const loginData = {
+            user_name: playerId,
+            password: password
+        };
+        // console.log(loginData);
+        
+        fetch(BASE_URL + '/login', {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(loginData)
+        })
+          .then(async response => {
+            if (!response.ok) {
+              setLoading(false);
+              let errorData;
+              try {
+                errorData = await response.json();
+              } catch (error) {
+                console.error('Error parsing JSON:', error);
+              }
+        
+              if (response.status === 422) {
+                setErrMsg("");
+                setError(errorData.errors);
+                // console.error(`Login failed with status ${response.status}:`, errorData);
+              }else if (response.status === 401) {
+                // console.error(`Login failed with status ${response.status}:`, errorData);
+                setError("");
+                setErrMsg(errorData.message)
+              }else{
+                // console.error(`Unexpected error with status ${response.status}`);
+              }
+        
+              throw new Error('Login Failed');
+            }
+        
+            return response.json();
+          })
+          .then(data => {
+            setData(data);
+            setLoading(false);
+            // console.log(data);
+            if (data.data.token) {
+              localStorage.setItem('token', data.data.token);
+              navigate('/');
+            } else {
+              throw new Error('Token not found in response');
+            }
+          })
+          .catch(error => {
+            // console.error('Login error:', error);
+          });
+        }
+
     return (
         <>
             <div className="d-flex justify-content-center align-items-center mb-5">
@@ -19,38 +96,60 @@ const Login = () => {
                     <img src={logo} alt="" />
                 </div> */}
                     <div className="text-center mt-sm-3 mb-3">
-                        <h5 className="gradient-text py-sm-3">အကောင့်ဝင်ရန်</h5>
+                        <h5 className="gradient-text py-sm-3">Login</h5>
                         <h3 className="text-light gradient-text d-block">Welcome Back!</h3>
                         <span className="gradient-text" style={{ fontSize: '12px' }}>welcome back we missed you</span>
                     </div>
-
-                    <Form className="mx-2" style={{ color: '#7d64a0' }}>
+                    {errMsg && (
+                        <div className="alert alert-danger text-white">
+                          *{errMsg}
+                        </div>
+                    )}
+                    <Form className="mx-2" style={{ color: '#7d64a0' }} onSubmit={login}>
                         {/* <Form.Group className="mb-3">
                         <Form.Select >
                         <option value="1"><img src={flag} alt="" /></option>
                         </Form.Select>
                     </Form.Group> */}
                         <Form.Group className="mb-3" controlId="formBasicEmail">
-                            <Form.Label style={{ fontSize: '14px' }}>ဖုန်းနံပါတ်</Form.Label>
-                            <Form.Control type="text" className="inputs" placeholder="03123456789" />
+                            <Form.Label style={{ fontSize: '14px' }}>PlayerID</Form.Label>
+                            <Form.Control 
+                            type="text" 
+                            className="inputs" 
+                            placeholder="LS01111" 
+                            onChange={(e)=>setPlayerId(e.target.value)}
+                            value={playerId}
+                            />
+                            {error.user_name && (
+                                <div className="text-danger">*{error.user_name}</div>
+                            )}
                         </Form.Group>
 
                         <Form.Group className="mb-3" controlId="formBasicPassword">
-                            <Form.Label style={{ fontSize: '14px' }}>လျှိ၀ှက်နံပါတ်</Form.Label>
-                            <Form.Control type="password" className="inputs" placeholder="Password" />
+                            <Form.Label style={{ fontSize: '14px' }}>Password</Form.Label>
+                            <Form.Control 
+                            type="password" 
+                            className="inputs" 
+                            placeholder="Password" 
+                            onChange={(e)=>setPassword(e.target.value)}
+                            value={password}
+                            />
+                            {error.password && (
+                                <span className="text-danger">*{error.password}</span>
+                            )}
                         </Form.Group>
 
-                        <span style={{ fontSize: '12px' }} >လျှိ၀ှက်နံပါတ် မမှတ်မိဘူးလား ? <Link to='/forget-password' >ဒီမှာနှိပ်ပါ</Link> </span>
+                        {/* <span style={{ fontSize: '12px' }} >Forgot Password? <Link to='/forget-password' ></Link> </span> */}
 
-                        <div className="d-flex justify-content-center mt-3">
+                        <div className="d-flex justify-content-center my-3">
                             <Button variant="primary" type="submit" className="d-block w-100" style={{ background: 'linear-gradient(to right, #cc45c4,#7944bc,#4277b6)' }}>
-                                အကောင့်၀င်မည်
+                                Login
                             </Button>
                         </div>
 
-                        <div className="text-center mt-4">
+                        {/* <div className="text-center mt-4">
                             <span style={{ fontSize: '12px' }}>အကောင့်အသစ်ဖွင့်ရန် <Link to='/register'>ဒီမှာနှိပ်ပါ</Link> </span>
-                        </div>
+                        </div> */}
                     </Form>
                 </div>
             </div>
