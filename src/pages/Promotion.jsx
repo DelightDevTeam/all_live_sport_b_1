2da33a784cf4603a5a211cdb3977c914fefa98f4
Wai Linn Kyaw <stephensuu03@gmail.com>
2024-04-28 10:49:02 +0630

    promotion integration

--- src/pages/Promotion.jsx
+++ src/pages/Promotion.jsx
@@ -1,23 +1,23 @@
 import React from "react";
 import '../assets/css/login.css';
-
-import promo1 from '../assets/img/promotion1.png';
-import promo2 from '../assets/img/promotion2.jpg';
-import promo3 from '../assets/img/promotion3.png';
+import useFetch from "../hooks/useFetch";
+import BASE_URL from "../hooks/baseURL";
 
 const Promotion = () => {
-    const promotions = [promo2, promo1, promo3];
+    const {data:promotions} = useFetch(BASE_URL + "/promotion");
+    console.log(promotions);
+
     return (
         <>
-            <div className="mt-5  text-center mb-5">
+            <div className="mt-5 pt-5 text-center mb-5">
                 <h3 className="my-3 gradient-text">PROMOTION</h3>
                 {
-                    promotions.map((promo) => {
-                        return (
-                            <img src={promo} alt="" className="w-100 rounded-lg my-2 px-sm-5 px-2" />
-                        );
-                    })
-                }
+                    promotions && promotions.map((promo, index) => (
+                        <div key={index}>
+                            <img src={promo.img_url} alt="" className="w-100 rounded-lg my-2 px-sm-5 px-2" />
+                        </div>
+                    )
+                )}
             </div>
         </>
     );
